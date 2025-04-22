d0a415664e11ac113402667f88401eb18b2d3325
Marysst <mariua22102004@gmail.com>
2025-04-22 03:07:28 +0300

    Fixed the brute_log_analyze_runtime.py file, fixed variable names

--- brute_log_analyze_runtime.py
+++ brute_log_analyze_runtime.py
@@ -12,27 +12,27 @@ def brute_force_discrete_log(alpha, beta, p, timeout=300):
             raise TimeoutError("Time limit exceeded ({} seconds)".format(timeout))
     return None, time.time() - start_time
 
-def save_to_csv(alpha, beta, p, x, runtime, problem_type, digit_length, filename="brute_log_results.csv"):
+def save_to_csv(alpha, beta, p, x, runtime, problem_type, order_prime_number, filename="brute_log_results.csv"):
     file_exists = os.path.isfile(filename)
     with open(filename, mode="a", newline="") as file:
         writer = csv.writer(file)
         if not file_exists:
             writer.writerow([
-                "problem_type", "digit_length", "alpha", "beta",
-                "prime_order_p", "result_x", "time_seconds"
+                "problem_type", "order_prime_number", "alpha", "beta",
+                "p", "result_x", "time_seconds"
             ])
         writer.writerow([
-            problem_type, digit_length, alpha, beta,
+            problem_type, order_prime_number, alpha, beta,
             p, x if x is not None else "timeout", runtime
         ])
 
 if __name__ == "__main__":
     if len(sys.argv) != 6:
-        print("Usage: python brute_logger.py <problem_type> <digit_length> <alpha> <beta> <p>")
+        print("Usage: python brute_logger.py <problem_type> <order_prime_number> <alpha> <beta> <p>")
         sys.exit(1)
 
     problem_type = int(sys.argv[1])
-    digit_length = int(sys.argv[2])
+    order_prime_number = int(sys.argv[2])
     alpha = int(sys.argv[3])
     beta = int(sys.argv[4])
     p = int(sys.argv[5])
@@ -44,7 +44,7 @@ if __name__ == "__main__":
         else:
             print("No solution found")
         print(f"Time taken: {runtime} seconds")
-        save_to_csv(alpha, beta, p, result, runtime, problem_type, digit_length)
+        save_to_csv(alpha, beta, p, result, runtime, problem_type, order_prime_number)
     except TimeoutError as e:
         print(e)
-        save_to_csv(alpha, beta, p, None, 300.0, problem_type, digit_length)
+        save_to_csv(alpha, beta, p, None, 300.0, problem_type, order_prime_number)
